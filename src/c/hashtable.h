35890f1de8d8334e65ea90844075a49c32f4f2a4
Rakin Uddin <rakin.uddin@mail.utoronto.ca>
2017-04-23 15:09:07 -0400

    Added struct declarations for hashtable object.

---
+++ src/c/hashtable.h
@@ -0,0 +1,12 @@
+#include <pebble.h>
+
+typedef struct _entry_s {		// Entry that will be held inside hashtable
+	char *key;					// Identifier of the value
+	char *value;
+	struct _linked_list_s *next;
+} entry_s;
+
+typedef struct _hash_table_s {
+	int size;			// Size of the hashtable
+	entry_s **table;	// Elements in table
+} hash_table_s;
