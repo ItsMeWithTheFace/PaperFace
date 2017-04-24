5966e4d2cf285f16d2cda2b258b7b98c8bfd5d4f
Rakin Uddin <rakin.uddin@mail.utoronto.ca>
2017-04-23 22:50:50 -0400

    Started developing APIs, made create_hashtable

---
+++ src/c/hashtable.c
@@ -0,0 +1,25 @@
+#include <pebble.h>
+#include "hashtable.h"
+
+hash_table_s *create_hash_table(int size) {
+	int i;
+	hash_table_s *new_table;
+	
+	// Allocate memory for the hashtable struct
+	if ((new_table = malloc(sizeof(hash_value_s))) == NULL) {
+		return NULL;
+	}
+	
+	// Allocate memory for the entries
+	if ((new_table->table = malloc(sizeof(list_t *) * size)) == NULL) {
+		return NULL;
+	}
+	
+	// Initialize elements of the table
+	for(i = 0; i < size; i++) new_table->table[i] = NULL;
+	
+	// Set the hashtable's size
+	new_table->size = size;
+	
+	return new_table;
+}
