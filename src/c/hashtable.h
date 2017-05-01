e3ca8f23b66d3e002f4f849258dcf8e3af9cbc82
Rakin Uddin <rakin.uddin@mail.utoronto.ca>
2017-04-30 21:39:48 -0400

    Formatting fixes

--- src/c/hashtable.h
+++ src/c/hashtable.h
@@ -1,14 +1,14 @@
 #include <pebble.h>
 
 typedef struct _entry_s {		// Entry that will be held inside hashtable
-	char *key;					// Identifier of the value
+	char *key;			// Identifier of the value
 	char *value;
 	struct _entry_s *next;
 } entry_s;
 
 typedef struct _hash_table_s {
 	int size;			// Size of the hashtable
-	entry_s **table;	// Elements in table
+	entry_s **table;		// Elements in table
 } hash_table_s;
 
 extern hash_table_s *create_hashtable(int size);
