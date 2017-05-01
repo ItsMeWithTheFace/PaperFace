eeeb2f7bfb58b982455908a658a012be3c77d880
Rakin Uddin <rakin.uddin@mail.utoronto.ca>
2017-04-30 21:38:31 -0400

    Added function calls to header file

--- src/c/hashtable.h
+++ src/c/hashtable.h
@@ -10,3 +10,9 @@ typedef struct _hash_table_s {
 	int size;			// Size of the hashtable
 	entry_s **table;	// Elements in table
 } hash_table_s;
+
+extern hash_table_s *create_hashtable(int size);
+extern unsigned int hash(hash_table_s *hashtable, char *key);
+extern entry_s *get_entry(hash_table_s *hashtable, char *key);
+extern int insert_keyvalue(hash_table_s *hashtable, char *key, char *value);
+extern void free_hashtable(hash_table_s *hashtable);
