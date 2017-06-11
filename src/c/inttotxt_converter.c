5e6d87036be43efa0f9365de67eb328578df97ec
Rakin Uddin <rakinuddin97@gmail.com>
2017-06-11 16:08:34 -0400

    added a few more key/value pairs and a destroy hashtables method

--- src/c/inttotxt_converter.c
+++ src/c/inttotxt_converter.c
@@ -22,12 +22,29 @@ void populate_hashtables(){
 	insert_keyvalue(ones, "7", "SEVEN");
 	insert_keyvalue(ones, "8", "EIGHT");
 	insert_keyvalue(ones, "9", "NINE");
-	insert_keyvalue(ones, "0", "CLOCK");
+	insert_keyvalue(ones, "0", "CLOCK");	// If minutes in the form of XX:X0
 	
 	// Insert tens conversions into hashtable
+	insert_keyvalue(tens, "0", "O'");		// if minutes in the form of XX:0X
 	insert_keyvalue(tens, "10", "TEN");
+	insert_keyvalue(tens, "11", "ELEVEN");
+	insert_keyvalue(tens, "12", "TWELVE");
+	insert_keyvalue(tens, "13", "THIRTEEN");
+	insert_keyvalue(tens, "14", "FOURTEEN");
+	insert_keyvalue(tens, "15", "FIFTEEN");
+	insert_keyvalue(tens, "16", "SIXTEEN");
+	insert_keyvalue(tens, "17", "SEVENTEEN");
+	insert_keyvalue(tens, "18", "EIGHTEEN");
+	insert_keyvalue(tens, "19", "NINETEEN");
 	insert_keyvalue(tens, "20", "TWENTY");
 	insert_keyvalue(tens, "30", "THIRTY");
 	insert_keyvalue(tens, "40", "FOURTY");
 	insert_keyvalue(tens, "50", "FIFTY");
 }
+
+void destroy_hashtables(){
+	free_hashtable(ones);
+	free_hashtable(tens);
+}
+
+
